ae8f92b1c71e67101b79426f57305cc32b133a45
Jun Miao <45115605+JunM1ao@users.noreply.github.com>
2021-10-29 22:43:48 -1000

    Update CoinFragment.java
    excess import

--- app/src/main/java/com/tools/tabletop/CoinFragment.java
+++ app/src/main/java/com/tools/tabletop/CoinFragment.java
@@ -1,6 +1,5 @@
 package com.tools.tabletop;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,9 +11,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.OnLifecycleEvent;
 
 import java.util.Random;
 
