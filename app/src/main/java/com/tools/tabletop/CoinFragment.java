94167a0ec05da65d11d13b9f0067450cd7d9ae9a
Jun Miao <45115605+JunM1ao@users.noreply.github.com>
2021-10-29 20:59:28 -1000

    Basic Coin Flip Implemented

--- app/src/main/java/com/tools/tabletop/CoinFragment.java
+++ app/src/main/java/com/tools/tabletop/CoinFragment.java
@@ -1,18 +1,47 @@
 package com.tools.tabletop;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Random;
+
 public class CoinFragment extends Fragment {
+    private int result = -1;
+    private final Random rand = new Random();
+    private Button flipBtn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_coin, container, false);
+        View v = inflater.inflate(R.layout.fragment_coin, container, false);
+
+        flipBtn = v.findViewById(R.id.coin_btn);
+        flipBtn.setOnClickListener(v1 -> {
+            if (v1.getId() != R.id.coin_btn) return;
+
+            int temp = rand.nextInt(2);
+            if (result != temp) {
+                result = temp;
+                if (temp == 1) {
+                    flipBtn.setBackgroundColor(Color.parseColor("#0984e3"));
+                    flipBtn.setText(getString(R.string.h));
+                } else {
+                    flipBtn.setBackgroundColor(Color.parseColor("#00cec9"));
+                    flipBtn.setText(getString(R.string.t));
+                }
+                Toast.makeText(v1.getContext(), "Coin Flipped!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return v;
     }
 }
--- app/src/main/java/com/tools/tabletop/DiceFragment.java
+++ app/src/main/java/com/tools/tabletop/DiceFragment.java
@@ -13,6 +13,7 @@ public class DiceFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dice, container, false);
+        View v = inflater.inflate(R.layout.fragment_dice, container, false);
+        return v;
     }
 }
